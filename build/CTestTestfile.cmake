5d20979c4efb04c53d2e96472fcfe2919ae94b02
Dechao JIANG <125193191+Cheney823@users.noreply.github.com>
2023-11-20 19:45:20 +0800

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,36 @@
+# CMake generated Testfile for 
+# Source directory: /home/cheney/24EME_FYP/src
+# Build directory: /home/cheney/24EME_FYP/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("myur_description")
+subdirs("robotiq/robotiq")
+subdirs("robotiq/robotiq_2f_140_gripper_visualization")
+subdirs("robotiq/robotiq_2f_85_gripper_visualization")
+subdirs("robotiq/robotiq_2f_c2_gripper_visualization")
+subdirs("robotiq/robotiq_3f_gripper_articulated_gazebo")
+subdirs("robotiq/robotiq_3f_gripper_articulated_msgs")
+subdirs("universal_robot/universal_robots")
+subdirs("robotiq/robotiq_ethercat")
+subdirs("robotiq/robotiq_2f_gripper_control")
+subdirs("robotiq/robotiq_ft_sensor")
+subdirs("robotiq/robotiq_modbus_rtu")
+subdirs("robotiq/robotiq_modbus_tcp")
+subdirs("robotiq/robotiq_2f_gripper_action_server")
+subdirs("robotiq/robotiq_3f_gripper_control")
+subdirs("robotiq/robotiq_3f_gripper_joint_state_publisher")
+subdirs("universal_robot/ur10_moveit_config")
+subdirs("universal_robot/ur10e_moveit_config")
+subdirs("universal_robot/ur16e_moveit_config")
+subdirs("universal_robot/ur3_moveit_config")
+subdirs("universal_robot/ur3e_moveit_config")
+subdirs("universal_robot/ur5_moveit_config")
+subdirs("universal_robot/ur5e_moveit_config")
+subdirs("universal_robot/ur_description")
+subdirs("universal_robot/ur_gazebo")
+subdirs("universal_robot/ur_kinematics")
+subdirs("robotiq/robotiq_3f_gripper_articulated_gazebo_plugins")
+subdirs("robotiq/robotiq_3f_gripper_visualization")
+subdirs("robotiq/robotiq_3f_rviz")
